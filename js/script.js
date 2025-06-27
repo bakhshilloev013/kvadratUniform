16b46a70f7ce098b428b29801ea2f98283477be8
Bekhzod Bakhshilloev <beki2627@gmail.com>
2025-06-27 21:25:55 +0500

    marquee finish

--- js/script.js
+++ js/script.js
@@ -13,24 +13,19 @@ AOS.init({
 // zoom-in, zoom-out
 // flip-left, flip-right
 
-/*
-    <div data-aos="fade-up">Элемент с анимацией</div>
-    <div data-aos="slide-right" data-aos-delay="100">Другой элемент</div>
-*/
+// Функционал плавной прокрутки по якорю
+const chevron = document.querySelector('.chevron');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        chevron.style.display = 'flex';
+    } else {
+        chevron.style.display = 'none';
+    }
 
-    // Функционал плавной прокрутки по якорю
-    const chevron = document.querySelector('.chevron');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
-            chevron.style.display = 'flex';
-        } else {
-            chevron.style.display = 'none';
-        }
-
-        chevron.addEventListener('click', (e) => {
-            e.preventDefault();
-            document.querySelector('#start').scrollIntoView({
-                behavior: 'smooth',
-            });
+    chevron.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('#start').scrollIntoView({
+            behavior: 'smooth',
         });
     });
+});
